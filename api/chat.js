b14779d91abe3a600132635fd3426a169fbf51cd
Souchan917 <h.so030502@gmail.com>
2025-02-01 06:04:48 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -198,12 +198,8 @@ module.exports = async (req, res) => {
                 model: "o1-mini-2024-09-12",
                 messages: [
                     {
-                        role: "system",
-                        content: config.prompt
-                    },
-                    {
                         role: "user",
-                        content: userMessage
+                        content: config.prompt + "\n\n" + userMessage
                     }
                 ],
                 max_completion_tokens: 200
