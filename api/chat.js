27549fdc3e453c585ae8322a282754de2192fd24
Souchan917 <h.so030502@gmail.com>
2025-02-01 05:56:14 +0900

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -206,7 +206,7 @@ module.exports = async (req, res) => {
                         content: userMessage
                     }
                 ],
-                max_completion_tokens: 100
+                max_completion_tokens: 200
             })
         });
         
